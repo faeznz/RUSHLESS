a2b966fa425aa701343e563897be4fa77ece3624
faeznz <faeznz@gmail.com>
2025-09-28 15:54:21 +0700

    feat: refactor start_app.bat to remove 'wt' and use separate cmd windows for each project

--- start_app.bat
+++ start_app.bat
@@ -1,8 +1,15 @@
 @echo off
+REM --- Script Jalankan Semua Project ---
 set BASEDIR=%~dp0
 
-wt ^
-  new-tab cmd /k "cd /d %BASEDIR%\RUSHLESS-BE-EXAM && npm install && node server.js" ^
-  ; new-tab cmd /k "cd /d %BASEDIR%\RUSHLESS-BE-MASTER && npm install && node server.js" ^
-  ; new-tab cmd /k "cd /d %BASEDIR%\RUSHLESS-FE && set PORT=4000 && npm install && npm start" ^
-  ; new-tab cmd /k "cloudflared tunnel run a6f8a35a-852c-4b78-acc3-65ab9c0bd04e"
+REM Buka BE-EXAM
+start cmd /k "cd /d %BASEDIR%\RUSHLESS-BE-EXAM && npm install && node server.js"
+
+REM Buka BE-MASTER
+start cmd /k "cd /d %BASEDIR%\RUSHLESS-BE-MASTER && npm install && node server.js"
+
+REM Buka FE
+start cmd /k "cd /d %BASEDIR%\RUSHLESS-FE && set PORT=4000 && npm install && npm start"
+
+REM Run Tunnel
+start cmd /k "cloudflared tunnel run a6f8a35a-852c-4b78-acc3-65ab9c0bd04e"
