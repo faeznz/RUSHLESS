fe5f7c7e7ef9d4f72e568afc005eeee374a8144b
faeznz <faeznz@gmail.com>
2025-09-28 16:06:45 +0700

    feat: improve comments in start_app.bat for clarity and consistency

--- start_app.bat
+++ start_app.bat
@@ -2,14 +2,14 @@
 REM --- Script Jalankan Semua Project ---
 set BASEDIR=%~dp0
 
-REM Buka BE-EXAM
-start cmd /k "cd /d %BASEDIR%\RUSHLESS-BE-EXAM && npm install && node server.js"
+REM --- Jalankan BE-EXAM ---
+start "" cmd /k "cd /d %BASEDIR%RUSHLESS-BE-EXAM && npm install && node server.js"
 
-REM Buka BE-MASTER
-start cmd /k "cd /d %BASEDIR%\RUSHLESS-BE-MASTER && npm install && node server.js"
+REM --- Jalankan BE-MASTER ---
+start "" cmd /k "cd /d %BASEDIR%RUSHLESS-BE-MASTER && npm install && node server.js"
 
-REM Buka FE
-start cmd /k "cd /d %BASEDIR%\RUSHLESS-FE && set PORT=4000 && npm install && npm start"
+REM --- Build & Serve FE ---
+start "" cmd /k "cd /d %BASEDIR%RUSHLESS-FE && npm install && npm run build && npx serve -s build -l 4000"
 
-REM Run Tunnel
-start cmd /k "cloudflared tunnel run a6f8a35a-852c-4b78-acc3-65ab9c0bd04e"
+REM --- Jalankan Cloudflare Tunnel ---
+start "" cmd /k "cloudflared tunnel run a6f8a35a-852c-4b78-acc3-65ab9c0bd04e"
